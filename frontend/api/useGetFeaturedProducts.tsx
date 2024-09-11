4e1ab693fe8ad783b27c49d5e5040ba7f4ef4421
pablo <pablo@pablo.pablo>
2024-09-11 18:53:35 +0200

    added carousel for images on main page

--- frontend/api/useGetFeaturedProducts.tsx
+++ frontend/api/useGetFeaturedProducts.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 export function useGetFeaturedProducts() {
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products`;
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/products?populate=*`;
   const [result, setResult] = useState(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState("");
--- frontend/components/featured-products.tsx
+++ frontend/components/featured-products.tsx
@@ -1,8 +1,10 @@
 "use client";
 import { useGetFeaturedProducts } from "@/api/useGetFeaturedProducts";
 import { ResponseType } from "@/types/response";
-import { Carousel, CarouselContent } from "./ui/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SkeletonSchema from "./skeletonSchema";
+import { ProductType } from "@/types/product";
+import { Card, CardContent } from "./ui/card";
 
 const FeaturedProducts = () => {
   const { loading, result }: ResponseType = useGetFeaturedProducts();
@@ -14,6 +16,28 @@ const FeaturedProducts = () => {
         <CarouselContent className="-ml-2 md:-ml-4">
           {loading && (
             <SkeletonSchema grid={3} />
+          )}
+          { result != null && (
+            result.map((product : ProductType) => {
+
+              const { attributes, id} = product;
+              const {slug, images, productName} = attributes;
+
+              return (
+                <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
+                  <div className="p-1">
+                    <Card className="py-4 border border-gray-200 shadow-none">
+                      <CardContent className="relative flex items-center justify-center px-6 py-2">
+                        <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`} alt="Image featured" />
+                        <div className="absolute w-full px-6 transition dura">
+
+                        </div>
+                      </CardContent>
+                    </Card>
+                  </div>
+                </CarouselItem>
+              )
+            })
           )}
         </CarouselContent>
       </Carousel>
