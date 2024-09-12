50e8e7b89955ccb7b34103c8e8ab3bdc3684c68d
manu <manuabril2001@outlook.es>
2024-09-12 15:39:42 +0200

    added funcionality popular products

--- frontend/components/featured-products.tsx
+++ frontend/components/featured-products.tsx
@@ -5,40 +5,67 @@ import { Carousel, CarouselContent, CarouselItem } from "./ui/carousel";
 import SkeletonSchema from "./skeletonSchema";
 import { ProductType } from "@/types/product";
 import { Card, CardContent } from "./ui/card";
+import { Expand, ShoppingCart } from "lucide-react";
+import IconButton from "./icon-button";
+import { useRouter } from "next/navigation";
 
 const FeaturedProducts = () => {
+  const router = useRouter();
   const { loading, result }: ResponseType = useGetFeaturedProducts();
-  console.log(result);
   return (
     <div className="max-w-6xl py-4 mx-auto sm:py-16 sm:px-24">
       <h3 className="px-6 text-3xl sm:pb-8">Popular products</h3>
       <Carousel>
         <CarouselContent className="-ml-2 md:-ml-4">
-          {loading && (
-            <SkeletonSchema grid={3} />
-          )}
-          { result != null && (
-            result.map((product : ProductType) => {
-
-              const { attributes, id} = product;
-              const {slug, images, productName} = attributes;
+          {loading && <SkeletonSchema grid={3} />}
+          {result != null &&
+            result.map((product: ProductType) => {
+              const { attributes, id } = product;
+              const { slug, images, productName } = attributes;
 
               return (
-                <CarouselItem key={id} className="md:basis-1/2 lg:basis-1/3 group">
+                <CarouselItem
+                  key={id}
+                  className="md:basis-1/2 lg:basis-1/3 group"
+                >
                   <div className="p-1">
                     <Card className="py-4 border border-gray-200 shadow-none">
                       <CardContent className="relative flex items-center justify-center px-6 py-2">
-                        <img src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`} alt="Image featured" />
-                        <div className="absolute w-full px-6 transition dura">
-
+                        <img
+                          src={`${process.env.NEXT_PUBLIC_BACKEND_URL}${images.data[0].attributes.url}`}
+                          alt="Image featured"
+                          className="object-fit:contain w-full h-48"
+                        />
+                        <div className="absolute w-full px-6 transition duration-200 opacity-0 group-hover:opacity-100"></div>
+                        <div className="flex justify-center gap-x-6">
+                          <IconButton
+                            onClick={() => router.push(`product/${slug}`)}
+                            icon={<Expand size={20} />}
+                            className="text-gray-600"
+                          />{" "}
+                          <IconButton
+                            onClick={() => console.log("Add item")}
+                            icon={<ShoppingCart size={20} />}
+                            className="text-gray-600"
+                          />
                         </div>
                       </CardContent>
+                      <div className="flex justify-between gap-4 px-8">
+                        <h3 className="text-lg font-bold">{productName}</h3>
+                        <div className="flex items-center justify-between gap-3">
+                          <p className="px-2 py-1 text-white bg-black rounded-full dark:bg-white dark:text-black w-fit">
+                            Prueba
+                          </p>
+                          <p className="px-2 py-1 text-white bg-red-700 rounded-full dark:bg-red-400 dark:text-black w-fit">
+                            Picante
+                          </p>
+                        </div>
+                      </div>
                     </Card>
                   </div>
                 </CarouselItem>
-              )
-            })
-          )}
+              );
+            })}
         </CarouselContent>
       </Carousel>
     </div>
